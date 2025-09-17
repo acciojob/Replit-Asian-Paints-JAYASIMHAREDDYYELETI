acd707ddfa917cc6599fd15c081a9f676b1c3c79
JAYASIMHAREDDYYELETI <jayayeleti44@gmail.com>
2025-09-17 11:39:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const form = document.getElementById("color-Form");
+    const boxes = document.querySelectorAll(".grid-item");
+    const resetBtn = document.getElementById("Reset");
+
+    form.addEventListener("submit", getAndApplyColor);
+    resetBtn.addEventListener("click", clearBC);
+
+    function getAndApplyColor(e) {
+      e.preventDefault();
+      const selectedBox = document.getElementById("block_id").value;
+      const selectedColor = document.getElementById("colour_id").value;
+
+      if (parseInt(selectedBox) > 0 && parseInt(selectedBox) < 10) {
+        form.reset();
+        clearBC();
+        const targetBox = document.querySelector(`[data-id='${selectedBox}']`);
+        targetBox.style.backgroundColor = selectedColor;
+        console.log(selectedBox, selectedColor);
+      } else {
+        alert("Enter valid Number between 1 and 9");
+      }
+    }
+
+    function clearBC() {
+      for (const box of boxes) {
+        box.style.backgroundColor = "transparent";
+      }
+    }
